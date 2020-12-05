8952b92419166a8b57dfc078aff394dc88f4f524
Goran Bunjevac <bunjevacgoran@gmail.com>
2020-12-05 21:20:33 +0100

    Style files organization

--- boggle-ui/src/app/features/single-player/single-player.component.ts
+++ boggle-ui/src/app/features/single-player/single-player.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-single-player',
   templateUrl: './single-player.component.html',
-  styleUrls: ['./single-player.component.scss']
 })
 export class SinglePlayerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- boggle-ui/src/app/features/splash/splash.component.ts
+++ boggle-ui/src/app/features/splash/splash.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.component.html',
-  styleUrls: ['./splash.component.scss']
 })
 export class SplashComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
