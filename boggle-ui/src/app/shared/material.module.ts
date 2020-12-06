a13296f8a86cb29920e925fe726da930b344c8ff
Goran Bunjevac <bunjevacgoran@gmail.com>
2020-12-06 11:14:02 +0100

    Add boggle board

--- boggle-ui/src/app/shared/material.module.ts
+++ boggle-ui/src/app/shared/material.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-
-
-
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
-  imports: [
-    MatButtonModule,
-    MatCardModule
-  ],
-  exports: [
-    MatButtonModule,
-    MatCardModule
-  ]
+  imports: [MatButtonModule, MatCardModule, MatGridListModule],
+  exports: [MatButtonModule, MatCardModule, MatGridListModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
