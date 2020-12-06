153dd6dc908c4098d642b151f1e734eea02a11e3
Goran Bunjevac <bunjevacgoran@gmail.com>
2020-12-06 20:56:38 +0100

    select dice and score calculation

--- boggle-ui/src/app/features/single-player/single-player.component.ts
+++ boggle-ui/src/app/features/single-player/single-player.component.ts
@@ -8,8 +8,13 @@ import { Subscription, timer } from 'rxjs';
   templateUrl: './single-player.component.html',
 })
 export class SinglePlayerComponent implements OnInit {
-  dices: Dice[];
+  dices: Array<Dice>;
+  selectedDices = new Array<Dice>();
+  wordsList = new Array<string>();
   isGameStarted: boolean = false;
+  lastSelectedDice: Dice;
+  score: number;
+
   countDown: Subscription;
   counter: number = 180;
   tick: number = 1000;
@@ -38,6 +43,7 @@ export class SinglePlayerComponent implements OnInit {
       if (this.counter === 0) {
         this.isGameStarted = false;
         this.resetCounter();
+        this.calculateScore();
       }
     });
     this.isGameStarted = true;
@@ -46,6 +52,61 @@ export class SinglePlayerComponent implements OnInit {
   resetCounter = () => {
     this.counter = 180;
     this.countDown.unsubscribe();
+  };
+
+  selectDice = (event: Event, dice: Dice) => {
+    if (this.isGameStarted) {
+      const selectedCard = event.target as HTMLElement;
+      const selectedCardLetter = selectedCard.getElementsByTagName('p');
+
+      selectedCard.classList.contains('selected-card')
+        ? selectedCard.classList.remove('selected-card')
+        : selectedCard.classList.add('selected-card');
+
+      const selectIsValid = this.selectValidation(dice);
+      if (selectIsValid) {
+        this.lastSelectedDice = dice;
+        this.selectedDices.push(dice);
+      }
+    }
+  };
+
+  getCurrentWord = () => {
+    return this.selectedDices?.reduce((acc, curr) => acc + curr.letter, '');
+  };
+
+  addToWordsList = () => {
+    this.wordsList.push(this.getCurrentWord());
+    this.resetBoard();
+  };
+
+  calculateScore = () => {
+    this._singlePlayerService.getScore(this.wordsList).subscribe((res) => (this.score = res));
+  };
+
+  private resetBoard = () => {
+    this.selectedDices = new Array<Dice>();
+    this.lastSelectedDice = null;
+  };
+
+  private selectValidation = (dice: Dice): boolean => {
+    if (!this.lastSelectedDice) {
+      return true;
+    }
+
+    const validX = [
+      this.lastSelectedDice.xPosition - 1,
+      this.lastSelectedDice.xPosition,
+      this.lastSelectedDice.xPosition + 1,
+    ];
+    const validY = [
+      this.lastSelectedDice.yPosition - 1,
+      this.lastSelectedDice.yPosition,
+      this.lastSelectedDice.yPosition + 1,
+    ];
+    return (
+      validX.some((x) => x === dice.xPosition) && validY.some((y) => y === dice.yPosition)
+    );
   };
 
   ngOnDestroy() {
--- boggle-ui/src/app/features/single-player/single-player.service.ts
+++ boggle-ui/src/app/features/single-player/single-player.service.ts
@@ -1,11 +1,26 @@
 import { Observable } from 'rxjs';
 import { Dice } from './models/dice';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable()
 export class SinglePlayerService {
   constructor(private _httpClient: HttpClient) {}
 
   getBoard = (): Observable<Dice[]> => this._httpClient.get<Dice[]>('/Board');
+
+  getScore = (words: Array<string>): Observable<number> => {
+    const params = this.getScoreParams(words);
+    return this._httpClient.get<number>('/Score', { params: params });
+  };
+
+  private getScoreParams = (words: Array<string>): HttpParams => {
+    let params = new HttpParams();
+    if (words) {
+      words.forEach((word, index) => {
+        params = params.set(`words[${index}]`, word);
+      });
+    }
+    return params;
+  };
 }
